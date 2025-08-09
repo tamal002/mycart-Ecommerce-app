1b38ab5982c7b4db0bbae5d0cf175eb486680ef5
Tamal saha <tamalsaha019@gmail.com>
2025-08-09 14:36:14 +0530

    shop-product-model-added

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    description = models.TextField()
+    launched_date = models.DateTimeField()
+
