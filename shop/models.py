900ec5bfe32e36f80cb60143dc229ff25423b2f5
Tamal saha <tamalsaha019@gmail.com>
2025-08-11 20:27:06 +0530

    model-is-modified

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    sub_category = models.CharField(max_length=50, default="")
     description = models.TextField()
-    launched_date = models.DateTimeField()
+    price = models.IntegerField(default=0)
+    launched_date = models.DateField()
+
+    def __str__(self):
+        return self.product_name
 
