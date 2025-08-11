c6e584aede0c684f11f72e3ab5fc313ab8a73607
Tamal saha <tamalsaha019@gmail.com>
2025-08-11 20:55:28 +0530

    media-necessory-settings-done

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='shop/images', default="")
     category = models.CharField(max_length=50, default="")
     sub_category = models.CharField(max_length=50, default="")
     description = models.TextField()
