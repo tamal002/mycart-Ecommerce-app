9613e2bc2bc8e63c253af3cc091efcd8a60a2365
Tamal saha <tamalsaha019@gmail.com>
2025-08-22 22:50:37 +0530

    displaying cart at navbar / other html page created

--- shop/views.py
+++ shop/views.py
@@ -14,16 +14,16 @@ def about_us(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    pass
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    pass
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    pass
+    return render(request, 'shop/search.html')
 
 def product_view(request):
-    pass
+    return render(request, 'shop/productview.html')
 
 def ckeckout(request):
-    pass
+    return render(request, 'shop/checkout.html')
